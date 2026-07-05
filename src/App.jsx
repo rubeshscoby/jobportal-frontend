737f41a319a238be985e0f31837d882345d71e6e
rubeshscoby <rubeshscooby26@gmail.com>
2026-07-05 21:35:03 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,18 @@ import RegisterPage from './RegisterPage'
 import LoginPage from './LoginPage'
 import JobListPage from './JobListPage'
 import ApplyJobPage from './ApplyJobPage'
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 function App() {
- 
-
   return (
     <BrowserRouter>
       <Routes>
-      <Route path='/register' element={<RegisterPage/>} />
-       <Route path='/login' element={<LoginPage/>} />
-        <Route path='/jobs' element={<JobListPage/>} />
-         <Route path='/apply/:jobId' element={<ApplyJobPage/>} />
+        <Route path='/' element={<Navigate replace to='/login' />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/jobs' element={<JobListPage />} />
+        <Route path='/apply/:jobId' element={<ApplyJobPage />} />
+        <Route path='*' element={<Navigate replace to='/login' />} />
       </Routes>
     </BrowserRouter>
   )
